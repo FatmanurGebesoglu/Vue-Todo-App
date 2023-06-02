7c504570c30de6de08cb15af8f522fd2676361b5
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2023-06-02 12:18:33 +0300

    json dosyası ve yapılacak componenti oluşturuldu

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,36 @@
 <template>
   <div class="home">
-   Todo App
+    <div v-if="yapilacaklar.length">
+      <div v-for="yap in yapilacaklar" :key="yap.id">
+        <Yapilacak :yapilacak="yap" />
+      </div>
+    </div>
+    <div v-else>
+      <p>Yapılacaklar yükleniyor</p>
+    </div>
   </div>
 </template>
 
 <script>
 
+import Yapilacak from '../components/Yapilacak.vue'
+
 export default {
   name: 'HomeView',
-  
+  components: {
+    Yapilacak
+  },
+  data() {
+    return {
+      yapilacaklar: [],
+    }
+  },
+  mounted() {
+    fetch('http://localhost:3000/yapilacaklar')
+      .then((res) => res.json())
+      .then((data) => this.yapilacaklar = data)
+      .catch((err) => console.log(err))
+  }
+
 }
 </script>
